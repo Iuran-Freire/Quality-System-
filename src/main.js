e48bfe8095a70a0b639bc816f7c04fc42fa92d72
Iuran Freire <iuranhumberto99@gmail.com>
2026-07-14 21:42:05 -0400

    ponto de corte

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import "./styles/main.css";
 import "./styles/design-system.css";
+import "./styles/modals.css";
 import "./styles/plans-table.css";
 import "./styles/alerts.css";
 import "./styles/shell.css";
